dd3db06a00e89fea52ce18c6ddfafe781af6f1c2
Tariq Mohammad <tariq@Tariqs-MacBook-Pro.local>
2017-03-22 15:30:08 +0800

    Commit

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Tariq Mohammad on 3/22/17.
+//  Copyright © 2017 Tariq Mohammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
